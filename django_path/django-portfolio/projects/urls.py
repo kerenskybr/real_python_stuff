59393b3f7aa4097f8a0d6a81339ce80bee0b1456
keresnkybr <roger.o.monteiro@gmail.com>
2021-04-27 15:54:47 -0300

    Add remaining django course

--- django_path/django-portfolio/projects/urls.py
+++ django_path/django-portfolio/projects/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from projects import views
 
+app_name = 'projects'
 
 urlpatterns = [
-    path('', views.project_list),
+    path('', views.all_projects, name='all_projects'),
+    #path('', views.project_list),
+    path('<int:pk>', views.project_detail, name='project_detail')
 ]
--- django_path/django-portfolio/projects/views.py
+++ django_path/django-portfolio/projects/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 #from django.http import HttpResponse
-
+from projects.models import Project
 # Create your views here.
-def project_list(request):
-    return render(request, 'projects/index.html')#HttpResponse("<h1>hell world</h1>")
+#def project_list(request):
+#    return render(request, 'projects/index.html')#HttpResponse("<h1>hell world</h1>")
+
+def all_projects(request):
+    projects = Project.objects.all()
+    return render(request, 'projects/all_projects.html', 
+        {'projects':projects})
+
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    return render(request, 'projects/detail.html',
+        {'project':project})
